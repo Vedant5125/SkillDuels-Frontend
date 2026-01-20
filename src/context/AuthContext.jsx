4c7d675737107d2348a010343f65493c5f66035d
Vedant5125 <vedantapraj5125@gmail.com>
2026-01-20 21:53:37 +0530

    deployment changes

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -16,7 +16,7 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [token, setToken] = useState(localStorage.getItem("accessToken"));
 
-  axios.defaults.baseURL = "http://localhost:5000";
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
   axios.defaults.withCredentials = true;
 
   if (token) {
